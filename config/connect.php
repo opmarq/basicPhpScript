3dc21fa76c77c55c6f883f153d5422450eb0da57
omar chajia <opmarq@gmail.com>
2017-12-26 20:33:37 +0000

    adding comments

---
+++ config/connect.php
@@ -0,0 +1,3 @@
+<?php
+
+//Connecting to the database
---
+++ config/dbqueries.php
@@ -0,0 +1,9 @@
+<?php
+
+// here goes database queries
+
+// select all categories 
+
+// select all questions with author and categories
+
+// select answers for a specifique question
