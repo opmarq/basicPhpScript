51e66e59030508152966c31ad140eae77eaad33f
omar chajia <opmarq@gmail.com>
2017-12-26 21:00:43 +0000

    creating the database connection

--- config/connect.php
+++ config/connect.php
@@ -1,3 +1,20 @@
 <?php
 
 //Connecting to the database
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+echo "Connected successfully";
+
+?>
--- config/dbqueries.php
+++ config/dbqueries.php
@@ -1,5 +1,41 @@
 <?php
 
+require_once("connect.php");
+
+function insertQuery($query,$connection){
+
+    if (mysqli_query($connection,$query)) {
+        
+        return true;
+
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        return false;
+    }
+
+}
+
+function checkAuth($username,$password,$connection)
+{
+    
+    $sql = "SELECT login FROM author where password = ". $password;  
+    
+    $result = mysqli_query($connection, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_object($result)) {
+
+        echo "id: " . $row->id. " - Name: " . $row->fullname."<br>";
+    }
+    } else {
+        echo "0 results";
+    }
+
+}
+
+checkAuth("omar","chajia");
+
 // here goes database queries
 
 // select all categories 
